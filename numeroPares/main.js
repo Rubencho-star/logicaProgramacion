adce6f94c0c180d8d41fb63c918d84d150243e5b
Rubencho-star <rudaher3@gmail.com>
2025-04-22 11:16:55 -0500

    ya muestra si es par multiplo de 5 o par y multiplo de 5

--- numeroPares/main.js
+++ numeroPares/main.js
@@ -1,10 +1,23 @@
 let calcular = document.getElementById("btn");
 let entrada = document.getElementById("entrada");
+let salida = document.getElementById("salida")
 let numero = 0;
+
 entrada.addEventListener("input",(evento) => {
     numero = evento.target.value;
     console.log(numero);
 });
+// ya tenemos el par, ahora vamos a poner que me diga si es par o miltiplo //
 calcular.addEventListener("click", ()=>{
-    console.log("calcular")
+    let hijo = document.createElement("li")
+    if(numero%2 == 0 && numero%5 == 0){
+        hijo.innerHTML = numero + "es par y multiplo de 5"
+    }else if(numero%2 == 0){
+        hijo.innerHTML = numero + " es par"
+    }else if (numero%5 == 0){   
+        hijo.innerHTML = numero + "es multiplo de 5" 
+    }else{
+        hijo.innerHTML = numero + "No es par ni multiplo de 5"   
+    }    
+    salida.appendChild(hijo)
 });
